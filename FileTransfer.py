28eae50d3e378b3ed605bbdc448f0b5bb290b1b2
ninj1an <104375571+ninj1an@users.noreply.github.com>
2022-05-16 13:12:30 -0400

    Add files via upload

---
+++ FileTransfer.py
@@ -0,0 +1,32 @@
+import os, shutil
+import pandas as pd
+
+os.getcwd()
+#read in the weekly edited mxd file and convert it to a list
+df = pd.read_csv("MXDDetailCircles.csv")
+changed_mxd = df['MXD'].tolist()
+
+#create a new list to change the .mxd files to pdf extentions
+new_list = []
+new_list = [i[:-4] for i in changed_mxd]
+
+#Create the list of PDFs to find in the folder
+pdf_list = [x + '.pdf' for i, x in enumerate(new_list)]
+
+#Loop through listed PDFs to find and move to the repair folder
+f = 0
+e = []
+while i < len(pdf_list):
+    try:
+        src_path = "K:/PDFRequestor/PRODRepository/pdf/in/" + pdf_list[f]
+        dst_path = "Z:/Maps/WebPDF/Repair/" + pdf_list[f]
+        shutil.move(src_path,dst_path)
+    except:
+        e.append(pdf_list[f])
+        pass
+    finally:
+        i = f + 1
+#The try/except/finally handles any 'file not found' errors by storing the missing PDF name and then moving on.
+
+print("All found files moved to repair folder.")
+print("Missing files: " + e)
