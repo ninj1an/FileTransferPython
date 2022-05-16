0ee4a34bccb38d6db3de29dc0b4a5c7dda8c3c51
Ian Stauffer <104375571+ninj1an@users.noreply.github.com>
2022-05-16 16:26:29 -0400

    Update FileTransfer.py

--- FileTransfer.py
+++ FileTransfer.py
@@ -8,25 +8,39 @@ changed_mxd = df['MXD'].tolist()
 
 #create a new list to change the .mxd files to pdf extentions
 new_list = []
-new_list = [i[:-4] for i in changed_mxd]
+new_list = [i[:-3] for i in changed_mxd]
 
 #Create the list of PDFs to find in the folder
-pdf_list = [x + '.pdf' for i, x in enumerate(new_list)]
+pdf_list = [x + 'pdf' for i, x in enumerate(new_list)]
 
 #Loop through listed PDFs to find and move to the repair folder
 f = 0
-e = []
+taketwo = []
 while i < len(pdf_list):
     try:
         src_path = "K:/PDFRequestor/PRODRepository/pdf/in/" + pdf_list[f]
         dst_path = "Z:/Maps/WebPDF/Repair/" + pdf_list[f]
         shutil.move(src_path,dst_path)
     except:
-        e.append(pdf_list[f])
+        taketwo.append(pdf_list[f])
         pass
     finally:
         i = f + 1
 #The try/except/finally handles any 'file not found' errors by storing the missing PDF name and then moving on.
+
+#Search the other folder for any missing files not found in the first location
+f = 0
+e = []
+while i < len(taketwo):
+    try:
+        src_path = "Z:/Maps/WebPDF/ReviewTransferredFromServer/" + taketwo[f]
+        dst_path = "Z:/Maps/WebPDF/Repair/" + taketwo[f]
+        shutil.move(src_path,dst_path)
+    except:
+        e.append(taketwo[f])
+        pass
+    finally:
+        i = f + 1
 
 #Create a data frame to export missing PDFs to a CSV
 missingPDF = {'PDF Not Found' : e}
@@ -36,4 +50,5 @@ df = pd.DataFrame(missingPDF)
 df.to_csv('MissingPDF.csv', index = False)
 
 print("All found files moved to repair folder.")
-print("Missing files: " + e)
+print("Missing files: ")
+print(e)
